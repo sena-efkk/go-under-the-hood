d77319a06b19390a4ec51525bbfdb2eaef1a8798
sena-efkk <senasultankar@gmail.com>
2026-03-06 00:12:10 +0300

    add if else

--- if-else/if.go
+++ if-else/if.go
@@ -18,4 +18,22 @@ func main() {
 
 	}
 
+	yas := 20
+	rol := "admin"
+
+	if yas > 18 && rol == "admin" {
+		fmt.Println("Tam erişim sağlandı")
+	} else if yas > 18 && rol == "user" {
+		fmt.Println("Sınırlı erişim")
+	} else {
+		fmt.Println("Erişim reddedildi.")
+	}
+	if puan := 85; puan > 80 {
+		fmt.Println("Bonus puan kazandınız")
+		//puan değişkeni o süslü parantezlerin ({ })
+		//içinde doğar ve kapanış parantezinde ölür.
+		//Sen dışarıdan ona seslendiğinde, derleyici
+		//"Böyle biri hiç yaşamadı" der
+	}
+
 }
